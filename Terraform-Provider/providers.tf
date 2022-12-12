74bc66aada765c0159f13102cdba8040d0616121
stanezil <fengting1995@gmail.com>
2022-12-12 20:05:32 +0800

    Update providers.tf

--- Terraform-Provider/providers.tf
+++ Terraform-Provider/providers.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aquasec" {
-  username = "xxxx@email.com" # POV to Update
+  username = "xxxx@email.com" # POV to Update --> Input email utilised to access the Aqua SaaS console 
   aqua_url = "https://asia-1.cloud.aquasec.com"
-  password = "xxxx" # POV to Update
+  password = "xxxx" # POV to Update --> Input pw utilised to access the Aqua SaaS console 
 }
